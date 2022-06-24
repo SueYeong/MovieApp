97e2e55432cfb0940cbe2ec0ccf0ede548f6e6fe
SueYeong <dudtb205@gmail.com>
2022-06-24 12:56:52 +0900

    api

---
+++ src/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "a79f68d5351aa49e6b1a879f49d037f6",
+    language: "ko-kr",
+  },
+});
+
+export const movieApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  topRated: () => api.get("movie/top_rated"),
+  latest: () => api.get("movie/latest"),
+  upComming: () => api.get("movie/upcoming"),
+};
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,3 +1,18 @@
+import { useEffect } from "react";
+import { movieApi } from "../../../api";
+
 export const Home = () => {
+  useEffect(() => {
+    const movieData = async () => {
+      const {
+        data: { results },
+      } = await movieApi.nowPlaying();
+      console.log(results[0].title);
+      // console.log(playing.date.results);
+      // console.log(await movieApi.nowPlaying());
+    };
+    movieData();
+  }, []);
+
   return <div>Home</div>;
 };
